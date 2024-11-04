7ca988d1ba7baf71336e5faa27f6b2f7137df01e
DanielKote <danielkote@gmail.com>
2024-11-04 12:31:05 -0500

    version change to 2.2.16.1 (bugfixing)

--- Foreman/Forms/MainForm.Designer.cs
+++ Foreman/Forms/MainForm.Designer.cs
@@ -552,7 +552,7 @@
             this.VersionLabel.Name = "VersionLabel";
             this.VersionLabel.Size = new System.Drawing.Size(53, 130);
             this.VersionLabel.TabIndex = 18;
-            this.VersionLabel.Text = "v2.2.16.0";
+            this.VersionLabel.Text = "v2.2.16.1";
             this.VersionLabel.TextAlign = System.Drawing.ContentAlignment.TopRight;
             // 
             // MainForm
